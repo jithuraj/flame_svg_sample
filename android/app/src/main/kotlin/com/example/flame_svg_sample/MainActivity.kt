774c857c130d852f150fc39634c93d28ac65a733
jithuraj <contact.jithuraj@gmail.com>
2023-03-28 19:40:47 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flame_svg_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flame_svg_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:flame/game.dart';
+import 'package:flame_svg/svg.dart';
+import 'package:flame_svg/svg_component.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GameWidget(game: MyGame()));
+}
+
+class MyGame extends FlameGame {
+  @override
+  FutureOr<void> onLoad() async {
+    final svgInstanceaa = await Svg.load('android.svg');
+    final size = Vector2.all(100);
+    final position = Vector2.all(100);
+    // code from docs
+    final svgComponent = SvgComponent.fromSvg(
+      size,
+      position,
+      svgInstanceaa,
+    );
+
+    /// change to fix the eroor
+    // final svgComponent = SvgComponent(
+    //   size: size,
+    //   position: position,
+    //   svg: svgInstanceaa,
+    // );
+
+    add(svgComponent);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    // code from docs
+    Svg svgInstance = Svg('android.svg');
+
+    final position = Vector2(100, 100);
+    final size = Vector2(300, 300);
+
+    svgInstance.renderPosition(canvas, position, size);
+    super.render(canvas);
+  }
+}
